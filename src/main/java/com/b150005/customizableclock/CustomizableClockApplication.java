6ca99ded284d8e1029b66c36b4822fec969f3de0
b150005 <b150005@outlook.jp>
2022-01-05 23:26:44 +0900

    first commit

---
+++ src/main/java/com/b150005/customizableclock/CustomizableClockApplication.java
@@ -0,0 +1,44 @@
+package com.b150005.customizableclock;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * JavaFX App
+ */
+public class CustomizableClockApplication extends Application {
+    public static void main(String[] args) {
+      launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+      // .fxmlファイルのロード
+      try {
+        URL url = CustomizableClockApplication.class.getResource("settings.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+  
+      // 終了時の処理
+      stage.showingProperty().addListener((observable, oldValue, newValue) -> {
+        if (oldValue == true && newValue == false) {
+          // 終了時に実行する処理
+        }
+      });
+      
+      // ウィンドウバーに表示するタイトル
+      stage.setTitle("Settings");
+
+      // ウィンドウの表示
+      stage.show();
+    }
+}
---
+++ src/main/java/com/b150005/customizableclock/SettingsController.java
@@ -0,0 +1,14 @@
+package com.b150005.customizableclock;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SettingsController implements Initializable {
+  @Override
+  public void initialize(URL url, ResourceBundle res) {
+    // TODO Auto-generated method stub
+    
+  }
+}
